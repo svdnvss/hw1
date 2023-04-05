371cb4cd745f9ef2ed0280b3babd09713840e71d
DESKTOP-5491RGM\Admin <nizamutdinovilyasn@gmail.com>
2023-04-06 02:04:37 +0500

    cleaning

--- src/helpers.ts
+++
@@ -1,9 +0,0 @@
-export const isNumber = (item: string): boolean => !isNaN(Number(item));
-
-export function getAllIndexes(arr, val) {
-    var indexes = [], i = -1;
-    while ((i = arr.indexOf(val, i+1)) != -1){
-        indexes.push(i);
-    }
-    return indexes;
-}
